f00e0c3496bb00c5e314c04acd4d47a5dd6d02be
pepperflavor <j8747j@naver.com>
2023-06-16 14:05:59 +0900

    first commit

---
+++ solidity/Crypto-Token-Contract.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+// 컴파일러 버전 명시 : 버전마다 구문 및 뉘앙스가 달라진다고 함
+// ^ : 컴파일 버전의 범위를 0이 아닌 소수점 다음 증분까지로 지정한다.
+
+// 클래스? 객체와 비슷하다고 보면 됨
+// 객체 : 상속 능력을 갖는 추상적 데이터 타입
+contract CryptoToken{
+
+}
+
+contract LearningSolidity{
+
+}
